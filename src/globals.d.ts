6ad6c1e640733791c4175a291d75b1db04895b60
Derek Worthen <worthend.derek@gmail.com>
2026-07-28 12:11:55 -0700

    Add JsonValue type.

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -3,3 +3,8 @@
  * `scripts/build.ts`) and `false` for dev/test via `bunfig.toml`'s `[define]`.
  */
 declare const IS_BINARY: boolean
+
+declare global {
+  type JsonPrimitive = string | number | boolean | null
+  type JsonValue = JsonPrimitive | JsonValue[] | { [key: string]: JsonValue }
+}
